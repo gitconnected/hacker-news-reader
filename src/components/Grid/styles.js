0d1f8bef26b5a3ea8e1d5c1fe27f10af0b8231f2
Trey Huffine <treyhuffine@gmail.com>
2018-10-03 09:53:02 -0400

    Large mode

--- src/components/Grid/styles.js
+++ src/components/Grid/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { mobile, tablet } from 'styles/mediaQueries';
+import { mobile, tablet, monitor } from 'styles/mediaQueries';
 
 export const GridWrapper = styled.ul`
   display: flex;
@@ -7,6 +7,10 @@ export const GridWrapper = styled.ul`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 32px;
+
+  ${monitor} {
+    grid-template-columns: repeat(4, 1fr);
+  }
 
   ${tablet} {
     grid-template-columns: repeat(2, 1fr);
--- src/styles/mediaQueries.js
+++ src/styles/mediaQueries.js
@@ -1,2 +1,3 @@
 export const mobile = '@media only screen and  (max-width: 480px)';
 export const tablet = '@media only screen and  (max-width: 768px)';
+export const monitor = '@media only screen and  (min-width: 1400px)';
