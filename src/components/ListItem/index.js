1387c1c8d600b569a72f274557b0413e4b6255e4
Brian Landers <brian@packetslave.com>
2018-12-19 17:19:22 -0800

    replace timeago.js with react-timeago (#17)

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Timeago from 'timeago.js';
+import TimeAgo from 'react-timeago';
 import getSiteHostname from 'utils/getSiteHostname';
 import getArticleLink, { HN_USER, HN_ITEM } from 'utils/getArticleLink';
 
 import { Item, Title, Host, ExernalLink, Description, CommentLink } from './styles';
-
-const timeago = Timeago();
 
 const ListItem = ({ by, kids = [], score, url, title, id, type, time }) => {
   const site = getSiteHostname(url) || 'news.ycombinator.com';
@@ -25,7 +23,7 @@ const ListItem = ({ by, kids = [], score, url, title, id, type, time }) => {
         <CommentLink href={`${HN_USER}${by}`} rel="nofollow noreferrer noopener" target="_blank">
           {by}
         </CommentLink>{' '}
-        {timeago.format(new Date(time * 1000).toISOString())} {' | '}
+        <TimeAgo date={new Date(time * 1000).toISOString()} />{' | '}
         <CommentLink href={commentUrl} rel="nofollow noreferrer noopener" target="_blank">
           {kids.length} Comments
         </CommentLink>
